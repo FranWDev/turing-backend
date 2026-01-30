1af4a9e84c7b41632fbec0b7ea67935ca2187854
FranWDev <hernandezcrosafrancisco@gmail.com>
2026-01-30 22:03:50 +0000

    Subido los ignores

--- target/classes/static/scripts/sidebar.js
+++
@@ -1,7 +0,0 @@
-import { initSidebar } from "./components/sidebar.component.js";
-
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", initSidebar);
-} else {
-  initSidebar();
-}
