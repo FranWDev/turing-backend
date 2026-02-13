b45d812d2d09ebc5c920b03baf5d588fbd7c3d2f
FranWDev <hernandezcrosafrancisco@gmail.com>
2026-02-13 09:25:25 +0000

    Reducida ram

--- src/main/java/com/economato/inventory/dto/projection/AllergenProjection.java
+++ src/main/java/com/economato/inventory/dto/projection/AllergenProjection.java
@@ -1,10 +1,5 @@
 package com.economato.inventory.dto.projection;
 
-/**
- * Proyección de interfaz para Allergen.
- * Spring Data JPA solo selecciona las columnas correspondientes a los getters
- * definidos.
- */
 public interface AllergenProjection {
 
     Integer getId();
